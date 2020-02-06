68fa0fd355c3274ebeb2bbfd0bce0ccc71cb29be
Evgeny Koval <evgenekoval@gmail.com>
2020-02-06 16:54:31 +0700

    stopSubmit, functional App, Redux store perceives on page refresh

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Header from "./Header";
-import {logout} from "../../redux/authReducer";
+import {getAuthUserData, logout} from "../../redux/authReducer";
 import {connect} from "react-redux";
 
 class HeaderContainer extends React.Component {
+    componentDidMount() {
+        this.props.getAuthUserData();
+    }
 
      render() {
         return (
@@ -16,4 +19,4 @@ const mapStateToProps = (state) => ({
     login: state.auth.login,
 });
 
-export default connect(mapStateToProps, {logout})(HeaderContainer);
+export default connect(mapStateToProps, {getAuthUserData, logout})(HeaderContainer);
