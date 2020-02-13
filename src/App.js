fab427f2fc6e1ed394cce71016ab93de18eeacdd
Evgeny Koval <evgenekoval@gmail.com>
2020-02-13 21:11:58 +0700

    React.lazy+suspense, with suspense HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import { Route, withRouter } from "react-router-dom";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
 import { connect } from "react-redux";
@@ -14,6 +12,13 @@ import Preloader from "./components/common/Preloader/Preloader";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "./redux/reduxStore";
+import { withSuspense } from "./hoc/withSuspense";
+
+// import ProfileContainer from "./components/Profile/ProfileContainer";
+// import DialogsContainer from "./components/Dialogs/DialogsContainer";
+
+const ProfileContainer = React.lazy(() => import("./components/Profile/ProfileContainer"));
+const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"));
 
 class App extends React.Component {
   componentDidMount() {
@@ -29,8 +34,8 @@ class App extends React.Component {
         <HeaderContainer />
         <Navbar friendList={[]} />
         <div className="appWrapperContent">
-          <Route path="/dialogs" render={() => <DialogsContainer />} />
-          <Route path="/profile/:userID?" render={() => <ProfileContainer />} />
+          <Route path="/dialogs" render={withSuspense(DialogsContainer)} />
+          <Route path="/profile/:userID?" render={withSuspense(ProfileContainer)} />
           <Route path="/users" render={() => <UsersContainer />} />
           <Route path="/login" render={() => <LoginPage />} />
         </div>
@@ -43,10 +48,7 @@ const mapStateToProps = state => ({
   initialized: state.app.initialized
 });
 
-let AppContainer = compose(
-  withRouter,
-  connect(mapStateToProps, { initializeApp })
-)(App);
+let AppContainer = compose(withRouter, connect(mapStateToProps, { initializeApp }))(App);
 
 const MainApp = props => {
   return (
